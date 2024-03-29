17a82806d4a176622fdbc4597449439f2f87e8e2
Siim Laasik <silaas@ttu.ee>
2024-03-29 17:02:20 +0200

    MovieRepository.java added

--- src/main/java/org/example/cgi_proovitoo/repository/MovieRepository.java
+++ src/main/java/org/example/cgi_proovitoo/repository/MovieRepository.java
@@ -1,7 +1,17 @@
 package org.example.cgi_proovitoo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    Movie findByMovieId(Integer id);
+    @Query("SELECT DISTINCT m FROM Movie m " +
+            "WHERE m.movieId NOT IN " +
+            "(SELECT s.movie.movieId FROM Session s " +
+            "WHERE s.sessionId IN " +
+            "(SELECT sg.sessionId FROM SessionGuest sg WHERE sg.profileId = :profileId))")
+    List<Movie> findMoviesNotSeenByProfileId(int profileId);
+
 }
+
